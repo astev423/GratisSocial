9f1c0515bb82771a43da6c2a4d6bb3432a428e6a
Alexander Stevens <alex.stvn2005@gmail.com>
2026-01-26 03:52:40 -0800

    refact: add space before return in function

--- lib/shared/sharedUtils.ts
+++ lib/shared/sharedUtils.ts
@@ -1,5 +1,6 @@
 export function formatDate(dateTimeString: string): string {
   const date = new Date(dateTimeString)
+
   return date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "2-digit",
