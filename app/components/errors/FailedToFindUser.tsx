428dd7f293694d279637078197f5aaa294ba3512
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-05 03:02:15 -0800

    refact: change text divs to more descriptive tags

--- app/components/errors/FailedToFindUser.tsx
+++ app/components/errors/FailedToFindUser.tsx
@@ -1,14 +1,14 @@
 export default function FailedToFindUser() {
   return (
     <div className="flex mt-20 justify-center">
-      <div className="items-center text-center text-5xl flex-col p-22 max-w-6xl bg-white font-bold">
+      <h2 className="items-center text-center text-5xl flex-col p-22 max-w-6xl bg-white font-bold">
         Failed to find user
-        <div className="text-2xl text-center font-normal mt-7">
+        <p className="text-2xl text-center font-normal mt-7">
           You must type in the username exactly as it's spelled.
           <br></br>
           No extra spaces and case sensitivity required.
-        </div>
-      </div>
+        </p>
+      </h2>
     </div>
   )
 }
