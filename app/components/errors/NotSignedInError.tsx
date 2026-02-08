3bf9816c1f2e53f1a2232fa5e860fab5a78cd62f
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-08 06:38:06 -0800

    fix: change another div to h2 for better accesibility and DX

--- app/components/errors/NotSignedInError.tsx
+++ app/components/errors/NotSignedInError.tsx
@@ -1,7 +1,7 @@
 export default function NotSignedInError() {
   return (
     <div className="flex flex-col items-center justify-center gap-15 mt-20">
-      <div className="font-bold text-2xl">You must sign in to see and create posts</div>
+      <h2 className="font-bold text-2xl">You must sign in to see and create posts</h2>
     </div>
   )
 }
