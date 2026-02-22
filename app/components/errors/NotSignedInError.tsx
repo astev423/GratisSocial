a5e33c49a044e45fdbcd748d13c934f3e903e5ca
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-22 06:37:18 -0800

    fix: update page to reduce unneeded css

--- app/components/errors/NotSignedInError.tsx
+++ app/components/errors/NotSignedInError.tsx
@@ -1,5 +1,6 @@
 export default function NotSignedInError() {
   return (
+    // Likely Don't need this div with all the css here
     <div className="flex flex-col items-center justify-center gap-15 mt-20">
       <h2 className="font-bold text-2xl">You must sign in to see and create posts</h2>
     </div>
