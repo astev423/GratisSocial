a727d9eca3492c4719ccd9ccb080b478a7e33dc3
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-24 05:26:23 -0800

    refact: minimize css and update tags for account created page

--- app/(pages)/account-created/page.tsx
+++ app/(pages)/account-created/page.tsx
@@ -4,13 +4,9 @@ export default async function AccountCreatedPage() {
   tryAddClerkUserToDb()
 
   return (
-    <div className="flex mt-20 justify-center">
-      <div className="items-center text-5xl flex-col p-22 max-w-6xl bg-white font-bold">
-        Account successfully created!
-        <div className="text-2xl text-center font-normal mt-5">
-          Enjoy your experience on an Open Source platform!
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="text-center text-5xl flex-col mt-20 font-bold">Account successfully created!</h1>
+      <p className="text-2xl text-center mt-10">Enjoy your experience on an Open Source platform!</p>
+    </>
   )
 }
