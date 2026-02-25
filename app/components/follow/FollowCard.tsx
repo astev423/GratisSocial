27ea46cf548940f31ed94d85a02a0529eebe7874
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-25 03:03:52 -0800

    fix: mark header with heading tag instead of div

--- app/components/follow/FollowCard.tsx
+++ app/components/follow/FollowCard.tsx
@@ -3,7 +3,7 @@ import FollowInfo from "./FollowInfo"
 export default function FollowCard() {
   return (
     <div className="flex gap-2 flex-col p-8 bg-white font-bold">
-      <div className="text-2xl">Follower Information</div>
+      <h2 className="text-2xl">Follower Information</h2>
       <div className="h-[17vh]">
         <FollowInfo />
       </div>
