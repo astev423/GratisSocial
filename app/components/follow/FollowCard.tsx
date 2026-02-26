f4bf71a6bdd3510ba85d7d883874d185c7161506
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-26 02:22:42 -0800

    fix: remove hardcoded width for part of follow card so original caller can set whole width instead

--- app/components/follow/FollowCard.tsx
+++ app/components/follow/FollowCard.tsx
@@ -4,7 +4,7 @@ export default function FollowCard() {
   return (
     <div className="flex gap-2 flex-col p-8 bg-white font-bold">
       <h2 className="text-2xl">Follower Information</h2>
-      <div className="h-[17vh]">
+      <div>
         <FollowInfo />
       </div>
     </div>
