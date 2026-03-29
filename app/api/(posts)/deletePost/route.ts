5791a91ad38d6b6b2d8a48ff5e2b82bf7ed8a35f
Alexander Stevens <alex.stvn2005@gmail.com>
2026-03-29 04:35:42 -0700

    refact: added more zod validation, logic getting repetition, function wrapper would help

--- app/api/(posts)/deletePost/route.ts
+++ app/api/(posts)/deletePost/route.ts
@@ -1,10 +1,28 @@
 import { NextResponse } from "next/server"
+import z from "zod"
 import { reqWithAuthWrapper } from "@/lib/server/api"
 import { deletePost } from "@/lib/server/dbQueries"
+
+const PostSchema = z
+  .object({
+    postId: z.string().trim().min(1, "PostID is required"),
+  })
+  .strict()
 
 export const DELETE = reqWithAuthWrapper(async (req, userId) => {
   const { postId } = (await req.json()) as { postId: string }
   await deletePost(postId, userId)
+  const result = PostSchema.safeParse(await req.json())
+
+  if (!result.success) {
+    return NextResponse.json(
+      {
+        message: "Invalid post payload",
+        errors: z.flattenError(result.error),
+      },
+      { status: 400 },
+    )
+  }
 
   return NextResponse.json({ status: 200 })
 })
